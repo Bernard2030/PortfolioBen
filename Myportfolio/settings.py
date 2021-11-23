c615d32e0d0272885a6e368de2c5b9d66cd66217
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-23 10:09:44 +0300

    add cloudinary

--- Myportfolio/settings.py
+++ Myportfolio/settings.py
@@ -13,6 +13,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import cloudinary
+import dj_database_url
+ 
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
